6bd7f4b64b9e0d731c607218e83d66aa606cc140
Yash Ghela <ghelayash@gmail.com>
2026-09-02 20:51:36 +0200

    taking a quick break btw parts

--- zine/frontend/src/main.jsx
+++ zine/frontend/src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {BrowserRouter, Routes, Route} from "react-router"
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Splash/>}/>
+  </Routes>
+  </BrowserRouter>
 )
